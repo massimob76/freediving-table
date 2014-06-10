8926a176dec292479f402c9b7a67dfa90a8fd59f
Massimo Battestini <massimob76@gmail.com>
2014-06-10 23:28:22 +0100

    TimeManager refactoring

--- spec/TimeManagerSpec.js
+++ spec/TimeManagerSpec.js
@@ -1,57 +1,55 @@
 describe ("TimeManager", function() {
 
+  var timeManager;
+
   beforeEach(function() {
-    TimeManager.setUp(100);
+    timeManager = new TimeManager(100);
   });
 
   it("should be able to create a new freediving table", function() {
-    expect(TimeManager.personalBest).toEqual(100);
-    expect(TimeManager.timeSpan).toEqual([150, 50, 135, 50, 120, 50, 105, 50, 90, 50, 75, 50, 60, 50, 60, 50]);
+    expect(timeManager.personalBest).toEqual(100);
+    expect(timeManager.timeSpan).toEqual([150, 50, 135, 50, 120, 50, 105, 50, 90, 50, 75, 50, 60, 50, 60, 50]);
   });
 
   it("should be able to shift to the next rest/holding time", function() {
-    var restHoldTime = [150, 50, 135, 50, 120, 50, 105, 50, 90, 50, 75, 50, 60, 50, 60, 50],
-        expected;
+    var expectedArray = [
+      { time: 150, remaining: 1195, total: 1195, status: "rest" },
+      { time:  50, remaining: 1045, total: 1195, status: "hold" },
+      { time: 135, remaining:  995, total: 1195, status: "rest" },
+      { time:  50, remaining:  860, total: 1195, status: "hold" },
+      { time: 120, remaining:  810, total: 1195, status: "rest" },
+      { time:  50, remaining:  690, total: 1195, status: "hold" },
+      { time: 105, remaining:  640, total: 1195, status: "rest" },
+      { time:  50, remaining:  535, total: 1195, status: "hold" },
+      { time:  90, remaining:  485, total: 1195, status: "rest" },
+      { time:  50, remaining:  395, total: 1195, status: "hold" },
+      { time:  75, remaining:  345, total: 1195, status: "rest" },
+      { time:  50, remaining:  270, total: 1195, status: "hold" },
+      { time:  60, remaining:  220, total: 1195, status: "rest" },
+      { time:  50, remaining:  160, total: 1195, status: "hold" },
+      { time:  60, remaining:  110, total: 1195, status: "rest" },
+      { time:  50, remaining:   50, total: 1195, status: "hold" },
+      { time:   0, remaining:    0, total: 1195, status: "done"}
+    ];
+    var expected;
 
-    while (expected = restHoldTime.shift()) {
-      expect(TimeManager.shift()).toEqual(expected);
+    while (expected = expectedArray.shift()) {
+      expect(timeManager.shift()).toEqual(expected);
     }
 
-    expect(TimeManager.shift()).toBeNull();
   });
+
 
   it("should be able to peek the current rest/holding time", function() {
-    expect(TimeManager.peek()).toEqual(150);
+    expect(timeManager.peek()).toEqual({ time: 150, remaining: 1195, total: 1195, status: "rest" });
 
-    TimeManager.shift();
-    expect(TimeManager.peek()).toEqual(50);
+    timeManager.shift();
+    expect(timeManager.peek()).toEqual({ time:  50, remaining: 1045, total: 1195, status: "hold" });
 
-    while(TimeManager.shift() != null);
-    expect(TimeManager.peek()).toBeNull();
+    while(timeManager.shift().time != 0);
+    expect(timeManager.peek()).toEqual({ time:   0, remaining:    0, total: 1195, status: "done" });
   });
 
-  it("should be able to check the current status", function() {
-    expect(TimeManager.status()).toEqual("rest");
 
-    TimeManager.shift();
-    expect(TimeManager.status()).toEqual("hold");
-
-    while(TimeManager.shift() != null);
-    expect(TimeManager.status()).toEqual("done");
-  });
-
-  it("should be able to return the remaing time", function() {
-    expect(TimeManager.remainingTime()).toEqual(1195);
-
-    TimeManager.shift();
-    expect(TimeManager.remainingTime()).toEqual(1045);
-
-    while(TimeManager.shift() != null);
-    expect(TimeManager.remainingTime()).toEqual(0);
-  });
-
-  it("should be able to return the total time", function() {
-    expect(TimeManager.totalTime()).toEqual(1195);
-  })
 
 });
--- src/TimeManager.js
+++ src/TimeManager.js
@@ -1,54 +1,41 @@
-var TimeManager = {
+function TimeManager(personalBest) {
+  var holdTime  = personalBest / 2,
+      timeSpan  = [],
+      REST_TIME = [150, 135, 120, 105, 90, 75, 60, 60];
 
-  REST_TIME: [150, 135, 120, 105, 90, 75, 60, 60],
-  personalBest: null,
-  timeSpan: [],
-  index: 0,
+  REST_TIME.forEach(function(el) {
+    timeSpan.push(el);
+    timeSpan.push(holdTime);
+  });
 
-  setUp: function(personalBest) {
+  this.personalBest = personalBest;
+  this.timeSpan = timeSpan;
+  this.totalTime = timeSpan.reduce(function(previous, current) {
+    return previous + current;
+  });
+  this.index = 0;
+};
 
-    var holdTime = personalBest / 2,
-        timeSpan = [];
+TimeManager.prototype.shift = function() {
+    var current = this.peek();
+    if (this.index != this.timeSpan.length) this.index++;
+    return current;
+};
 
-    this.REST_TIME.forEach(function(el) {
-      timeSpan.push(el);
-      timeSpan.push(holdTime);
-    });
+TimeManager.prototype.peek = function() {
+  return (this.index == this.timeSpan.length) ?
+    { time: 0, remaining: 0, total: 1195, status: "done" } :
+  { time:      this.timeSpan[this.index],
+   remaining: this._remainingTime(),
+   total:     this.totalTime,
+   status:    (this.index % 2 == 0) ? "rest" : "hold"
+  };
+};
 
-    this.personalBest = personalBest;
-    this.timeSpan = timeSpan;
-    this.index = 0;
-  },
-
-  shift: function() {
-    if (this.index == this.timeSpan.length) return null;
-    return this.timeSpan[this.index++];
-  },
-
-  peek: function() {
-    if (this.index == this.timeSpan.length) return null;
-    return this.timeSpan[this.index];
-  },
-
-  status: function() {
-    if (this.index == this.timeSpan.length) return "done";
-    return (this.index % 2 == 0) ? "rest" : "hold";
-  },
-
-  remainingTime: function() {
-    var remaining = 0;
-    for (var i = this.index; i < this.timeSpan.length; i++) {
-      remaining += this.timeSpan[i];
-    }
-    return remaining;
-  },
-
-  totalTime: function() {
-    return this.timeSpan.reduce(function(previous, current) {
-      return previous + current;
-    });
+TimeManager.prototype._remainingTime = function() {
+  var remaining = 0;
+  for (var i = this.index; i < this.timeSpan.length; i++) {
+    remaining += this.timeSpan[i];
   }
-
-
-
+  return remaining;
 }
